0d53ee310dfbfdcff5f1dd5e0688382562d522ce
Pablo Fernandez <fernandezpablo85@gmail.com>
2023-08-04 19:05:22 -0300

    improve cli interface, add tini to docker compose

--- 1_consistent_hashing/src/main.rs
+++ 1_consistent_hashing/src/main.rs
@@ -6,26 +6,44 @@ use std::time::Instant;
 // On a 32gb Apple M1 Pro
 // A cost of 12 takes about ~1 second
 // A cost of 15 takes about ~7 seconds
-const BCRYPT_COST: u32 = 16;
+const BCRYPT_COST: u32 = 15;
 const SERVER_PORT: u32 = 8888;
 
-enum Command<'a> {
+#[derive(Parser, PartialEq, Eq, Debug)]
+enum Command {
+    #[command(about = "Ping random server")]
     Ping,
-    Hash(&'a str),
-    Exit,
-    Err,
+    #[command(about = "Generate bcrypt hash from plaintext")]
+    Hash { plain: String },
+    #[command(about = "Run in server mode")]
+    Server,
 }
 
-#[derive(Parser, Debug)]
-struct Args {
-    #[arg(short, long)]
-    /// Run in server mode
-    server: bool,
+impl Command {
+    fn new(input: &str) -> Self {
+        let parts: Vec<&str> = input.trim().split(" ").collect();
+        match parts[0].to_uppercase().as_str() {
+            "PING" => Command::Ping,
+            "HASH" if parts.len() > 1 => Command::Hash {
+                plain: parts[1].to_string(),
+            },
+            _ => {
+                panic!("can't create a command from input {}", input);
+            }
+        }
+    }
+}
+
+#[derive(Parser)]
+#[clap(version = "1.0", author = "Pablo Fernandez")]
+struct Cli {
+    #[clap(subcommand)]
+    cmd: Command,
 }
 
 fn main() {
-    let args = Args::parse();
-    if args.server {
+    let args = Cli::parse();
+    if args.cmd == Command::Server {
         server()
     } else {
         client(args)
@@ -35,41 +53,34 @@ fn main() {
 fn server() {
     let addr = format!("0.0.0.0:{SERVER_PORT}");
     let listener = TcpListener::bind(&addr);
-    match listener {
-        Err(err) => {
-            eprintln!("can't connect to port {SERVER_PORT}, err: {:?}", err)
-        }
-        Ok(listener) => {
-            println!("listening on {addr}");
-            for stream in listener.incoming() {
-                match stream {
-                    Ok(req) => handle_req(req),
-                    Err(err) => eprintln!("error while receiving req: {}", err),
-                }
-            }
+    assert!(listener.is_ok(), "can't bind to port {}", SERVER_PORT);
+    let listener = listener.unwrap();
+    println!("listening on {addr}");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(req) => handle_req(req),
+            Err(err) => eprintln!("error while receiving req: {}", err),
         }
     }
 }
 
 fn handle_req(mut socket: TcpStream) {
     let mut buffer = vec![0; 256];
-    socket.read(&mut buffer).expect("couldn't read from buffer");
+    let read = socket.read(&mut buffer).expect("couldn't read from buffer");
 
-    let request = String::from_utf8_lossy(&buffer[..]);
-    let command = parse(request.as_ref());
+    let request = String::from_utf8_lossy(&buffer[..read]);
+    let command = Command::new(request.as_ref());
+    println!("request: {}", request);
     match command {
         Command::Ping => {
             socket.write(b"PONG\n").unwrap();
         }
-        Command::Hash(plain) => {
-            let h = hash(plain);
+        Command::Hash { plain } => {
+            let h = hash(&plain);
             socket.write(format!("{h}\n").as_bytes()).unwrap();
         }
-        Command::Exit => panic!("EXIT"),
-        Command::Err => {
-            socket
-                .write(format!("ERR: {}\n", request).as_bytes())
-                .unwrap();
+        Command::Server => {
+            panic!("this should never happen")
         }
     }
     socket.flush().unwrap()
@@ -85,17 +96,6 @@ fn hash(plain: &str) -> String {
     hash
 }
 
-fn parse(req: &str) -> Command {
-    let parts: Vec<&str> = req.trim().split(" ").collect();
-    let cmd = parts[0];
-    match cmd.to_uppercase().as_str() {
-        "PING" => Command::Ping,
-        "HASH" if parts.len() > 1 => Command::Hash(parts[1]),
-        "EXIT" => Command::Exit,
-        _ => Command::Err,
-    }
-}
-
-fn client(_args: Args) {
-    println!("running in client mode")
+fn client(cli: Cli) {
+    println!("running in client mode: {:?}", cli.cmd)
 }
