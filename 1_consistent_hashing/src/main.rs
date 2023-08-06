3a0e0eb02f9c1e44fbf12b4b3ac412dc2465a906
Pablo Fernandez <fernandezpablo85@gmail.com>
2023-08-06 18:13:37 -0300

    separate server.rs

--- 1_consistent_hashing/src/main.rs
+++ 1_consistent_hashing/src/main.rs
@@ -1,16 +1,10 @@
 use clap::Parser;
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::time::Instant;
-
-// On a 32gb Apple M1 Pro
-// A cost of 12 takes about ~1 second
-// A cost of 15 takes about ~7 seconds
-const BCRYPT_COST: u32 = 15;
-const SERVER_PORT: u32 = 8888;
+use std::fs::File;
+use std::io::prelude::*;
+mod server;
 
 #[derive(Parser, PartialEq, Eq, Debug)]
-enum Command {
+pub enum ClientCommand {
     #[command(about = "Ping random server")]
     Ping,
     #[command(about = "Generate bcrypt hash from plaintext")]
@@ -19,83 +13,43 @@ enum Command {
     Server,
 }
 
-impl Command {
-    fn new(input: &str) -> Self {
-        let parts: Vec<&str> = input.trim().split(" ").collect();
-        match parts[0].to_uppercase().as_str() {
-            "PING" => Command::Ping,
-            "HASH" if parts.len() > 1 => Command::Hash {
-                plain: parts[1].to_string(),
-            },
-            _ => {
-                panic!("can't create a command from input {}", input);
-            }
-        }
-    }
-}
-
 #[derive(Parser)]
 #[clap(version = "1.0", author = "Pablo Fernandez")]
-struct Cli {
+pub struct Cli {
     #[clap(subcommand)]
-    cmd: Command,
+    pub cmd: ClientCommand,
 }
 
 fn main() {
     let args = Cli::parse();
-    if args.cmd == Command::Server {
-        server()
+    if args.cmd == ClientCommand::Server {
+        server::server()
     } else {
+        let nodes = parse_nodes_json();
         client(args)
     }
 }
 
-fn server() {
-    let addr = format!("0.0.0.0:{SERVER_PORT}");
-    let listener = TcpListener::bind(&addr);
-    assert!(listener.is_ok(), "can't bind to port {}", SERVER_PORT);
-    let listener = listener.unwrap();
-    println!("listening on {addr}");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(req) => handle_req(req),
-            Err(err) => eprintln!("error while receiving req: {}", err),
-        }
-    }
-}
-
-fn handle_req(mut socket: TcpStream) {
-    let mut buffer = vec![0; 256];
-    let read = socket.read(&mut buffer).expect("couldn't read from buffer");
-
-    let request = String::from_utf8_lossy(&buffer[..read]);
-    let command = Command::new(request.as_ref());
-    println!("request: {}", request);
-    match command {
-        Command::Ping => {
-            socket.write(b"PONG\n").unwrap();
-        }
-        Command::Hash { plain } => {
-            let h = hash(&plain);
-            socket.write(format!("{h}\n").as_bytes()).unwrap();
-        }
-        Command::Server => {
-            panic!("this should never happen")
-        }
-    }
-    socket.flush().unwrap()
-}
-
-fn hash(plain: &str) -> String {
-    println!("hashing: {}", plain);
-    let tic = Instant::now();
-    let hash = bcrypt::hash(plain, BCRYPT_COST).unwrap();
-    let toc = tic.elapsed().as_millis();
-    println!("result:  {}", hash);
-    println!("elapsed: {}ms", toc);
-    hash
-}
-
 fn client(cli: Cli) {
-    println!("running in client mode: {:?}", cli.cmd)
+    match cli.cmd {
+        ClientCommand::Ping => {
+            let node = random_node();
+            send(node, "PING");
+        }
+        ClientCommand::Hash { plain } => {
+            let node = random_node();
+            send(node, format!("HASH {}", plain));
+        }
+        ClientCommand::Server => {
+            panic!("can't happen, we're running in client mode")
+        }
+    }
+}
+
+fn random_node() -> String {
+    let mut file = File::open("nodes.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let nodes: Vec<Node> = serde_json::from_str(&contents).expect("Error parsing the file");
 }
---
+++ 1_consistent_hashing/src/server.rs
@@ -0,0 +1,76 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::time::Instant;
+
+// On a 32gb Apple M1 Pro
+// A cost of 12 takes about ~1 second
+// A cost of 15 takes about ~7 seconds
+const BCRYPT_COST: u32 = 15;
+const SERVER_PORT: u32 = 8888;
+
+enum ServerCommand {
+    Ping,
+    Hash(String),
+}
+
+impl ServerCommand {
+    fn new(input: &str) -> Option<Self> {
+        if input.trim().len() == 0 {
+            return None;
+        }
+        let parts: Vec<&str> = input.trim().split(" ").collect();
+        match parts[0].to_uppercase().as_str() {
+            "PING" => Some(ServerCommand::Ping),
+            "HASH" if parts.len() > 1 => Some(ServerCommand::Hash(parts[1].to_string())),
+            _ => None,
+        }
+    }
+}
+
+pub fn server() {
+    let addr = format!("0.0.0.0:{SERVER_PORT}");
+    let listener = TcpListener::bind(&addr);
+    assert!(listener.is_ok(), "can't bind to port {}", SERVER_PORT);
+    let listener = listener.unwrap();
+    println!("listening on {addr}");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(req) => handle_req(req),
+            Err(err) => eprintln!("error while receiving req: {}", err),
+        }
+    }
+}
+
+fn handle_req(mut socket: TcpStream) {
+    let mut buffer = vec![0; 256];
+    let read = socket.read(&mut buffer).expect("couldn't read from buffer");
+
+    let request = String::from_utf8_lossy(&buffer[..read]);
+    let command = ServerCommand::new(request.as_ref());
+    println!("request: {}", request);
+    match command {
+        Some(ServerCommand::Ping) => {
+            socket.write(b"PONG\n").unwrap();
+        }
+        Some(ServerCommand::Hash(plain)) => {
+            let h = hash(&plain);
+            socket.write(format!("{h}\n").as_bytes()).unwrap();
+        }
+        None => {
+            socket
+                .write(format!("unknown command: {}\n", request).as_bytes())
+                .unwrap();
+        }
+    }
+    socket.flush().unwrap()
+}
+
+fn hash(plain: &str) -> String {
+    println!("hashing: {}", plain);
+    let tic = Instant::now();
+    let hash = bcrypt::hash(plain, BCRYPT_COST).unwrap();
+    let toc = tic.elapsed().as_millis();
+    println!("result:  {}", hash);
+    println!("elapsed: {}ms", toc);
+    hash
+}
